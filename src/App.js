3af68ffb267c026af8c447e02fd8041c016a348e
Geraldyn Chirinos <geraldynchidesousa@gmail.com>
2021-06-09 16:23:07 -0300

    Refactorizando varios archivos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import SearchBar from './Components/SearchBar/search.container'
+import SearchBar from './modules/SearchBar/search.container'
 import './commonStyles/global.scss'
+import SearchResults from './modules/SearchResults/results.container'
 
 const App = () => (
-  <>
+  <main>
+    <SearchBar />
     <Router>
       <Switch>
-        <Route exact path="/" component={SearchBar}></Route>
+        <Route exact path="/" component={SearchResults}></Route>
       </Switch>
     </Router>
-  </>
+  </main>
 )
 
 export default App

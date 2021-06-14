59fbd659460ca433f493d1b6b748a93fb0059763
Geraldyn Chirinos <geraldynchidesousa@gmail.com>
2021-06-13 21:30:48 -0300

    terminado el listado de categorias de detalle de producto en la parte del back

--- src/services/items.getDetails.js
+++ src/services/items.getDetails.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const { getInteger, getDecimal, authorInfo } = require('./utils')
 
 const urlItem = 'https://api.mercadolibre.com/items';
+const urlCategories = 'https://api.mercadolibre.com/categories'
 
 const parseResponseDetails = res => ({
   id: res.id,
@@ -11,7 +12,7 @@ const parseResponseDetails = res => ({
     amount: getInteger(res.price),
     decimals: getDecimal(res.price)
   },
-  categories: ['Memoria','Flores','Disfraces','Halloween'],
+  categories: res.categories.map(c => c.name),
   picture: res.pictures[0].secure_url || res.secure_thumbnail,
   condition: res.condition,
   free_shipping: res.shipping.free_shipping,
@@ -22,11 +23,18 @@ const parseResponseDetails = res => ({
 const getItemDetails = async param => {
   const responseListedItemPromise = axios.get(`${urlItem}/${param}`);
   const responseDescriptionItemPromise = axios.get(`${urlItem}/${param}/description`);
+
   const [responseListedItem, responseDescriptionItem] = await Promise.all([responseListedItemPromise, responseDescriptionItemPromise])
+
+  const categories = await axios.get(`${urlCategories}/${responseListedItem.data.category_id}`);
 
   return ({
     author: authorInfo,
-    item: parseResponseDetails({ ...responseListedItem.data, description: responseDescriptionItem.data.plain_text })
+    item: parseResponseDetails({
+      ...responseListedItem.data,
+      description: responseDescriptionItem.data.plain_text,
+      categories: categories.data.path_from_root
+    })
   })
 }
 

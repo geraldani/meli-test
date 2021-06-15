412b010ad669b4e3b64fa05d482bc6b572be2831
Geraldyn Chirinos <geraldynchidesousa@gmail.com>
2021-06-14 22:06:34 -0300

    Poniendo mas bonito el loader

--- src/Components/Loaders/loading.component.js
+++ src/Components/Loaders/loading.component.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import BarSpinner from './barLoader'
-import './styles.scss';
+import './styles.scss'
 
 /**
  * Un componente simple que muetra la palabra cargando cuando esta cargando alguna parte de la app
@@ -10,10 +10,12 @@ import './styles.scss';
 const Loading = () => {
   return (
     <div className="loader-container">
-      <span className="loader-message">Cargando...</span>
-      <BarSpinner />
+      <div className="loader-content">
+        <span className="loader-message">Cargando...</span>
+        <BarSpinner />
+      </div>
     </div>
   )
 }
 
-export default Loading;
+export default Loading
